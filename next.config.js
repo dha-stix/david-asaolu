5cba25f38d60781c8e19440590ba8ddab752284d
David Asaolu <67129211+dha-stix@users.noreply.github.com>
2024-01-25 17:16:36 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/practicaldev/**',
-      },
-    ],
-  },
-}
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "media.dev.to",
+				port: "",
+			},
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
